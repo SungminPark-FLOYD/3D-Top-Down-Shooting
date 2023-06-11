cfca62fa52b800be5383a00d5e880069eabfde93
FLOYD-psm <107343662+SungminPark-FLOYD@users.noreply.github.com>
2023-06-11 12:05:14 +0900

    script
    script

---
+++ Script/Bullet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage;
+    public bool isMelee;
+    public bool isRock;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!isRock && collision.gameObject.tag == "Floor")
+        {
+            //3초 뒤 삭제
+            Destroy(gameObject, 3);
+        }
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isMelee && other.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
